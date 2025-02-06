41193fd4d493159caf0471c812b71912272c130b
Tomasz Adamczyk <tpadamczyk7@gmail.com>
2025-02-06 14:57:00 +0100

    feat: add datadog

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,14 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+// Note: it is recommended to use a matching version with RUM SDK if used.
+import { datadogLogs } from "@datadog/browser-logs";
+
+datadogLogs.init({
+  clientToken: process.env.NEXT_PUBLIC_DATADOG_CLIENT_TOKEN!,
+  site: "datadoghq.eu",
+  forwardErrorsToLogs: true,
+  sessionSampleRate: 100,
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
+import { useEffect } from "react";
+import { datadogLogs } from "@datadog/browser-logs";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -12,6 +14,10 @@ const geistMono = Geist_Mono({
 });
 
 export default function Home() {
+  useEffect(() => {
+    datadogLogs.logger.info("Test", { name: "name", id: 123 });
+  }, []);
+
   return (
     <div
       className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
